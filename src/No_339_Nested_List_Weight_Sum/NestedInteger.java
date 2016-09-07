f024944c6b4d45adcab2ca436bddcb3dcad68bdc
testhuazhe <ytdxyinhuazhe@sina.com>
2016-09-07 16:13:10 -0400

    one more easy answer

---
+++ src/No_339_Nested_List_Weight_Sum/NestedInteger.java
@@ -0,0 +1,22 @@
+package No_339_Nested_List_Weight_Sum;
+
+import java.util.List;
+
+public class NestedInteger {
+
+	public boolean isInteger() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	public int getInteger() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public List<NestedInteger> getList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
