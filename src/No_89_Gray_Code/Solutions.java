ade3b3c4fd13e06a5af35942b2213afb9060d76e
testhuazhe <ytdxyinhuazhe@sina.com>
2016-09-24 15:28:24 -0400

    Sep 23th

---
+++ src/No_89_Gray_Code/Solutions.java
@@ -0,0 +1,14 @@
+package No_89_Gray_Code;
+
+import java.util.List;
+
+public class Solutions {
+
+	public static void main(String[] args) {
+	}
+	
+	public static List<Integer> grayCode(int n){
+		
+		return null;
+	}
+}
